3bae29f7f60e878ff115b8012d44e73e5f65c725
Vijay Konduru <konduruvijaykumar@gmail.com>
2020-05-09 21:46:13 +0530

    Initial code commit

---
+++ springboot-hystrix-service-1/src/main/java/org/pjay/hystrix/SpringbootHystrixService1Application.java
@@ -0,0 +1,13 @@
+package org.pjay.hystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHystrixService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootHystrixService1Application.class, args);
+	}
+
+}
---
+++ springboot-hystrix-service-1/src/test/java/org/pjay/hystrix/SpringbootHystrixService1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pjay.hystrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootHystrixService1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
