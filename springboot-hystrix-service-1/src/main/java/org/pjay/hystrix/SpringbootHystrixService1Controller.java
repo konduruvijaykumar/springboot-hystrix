34dcfeaf06cb07995a4e369b4aa367382b6021b3
Vijay Konduru <konduruvijaykumar@gmail.com>
2020-05-09 22:05:26 +0530

    added rest controller

---
+++ springboot-hystrix-service-1/src/main/java/org/pjay/hystrix/SpringbootHystrixService1Controller.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package org.pjay.hystrix;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author vijayk
+ *
+ */
+@RestController
+public class SpringbootHystrixService1Controller {
+
+}
