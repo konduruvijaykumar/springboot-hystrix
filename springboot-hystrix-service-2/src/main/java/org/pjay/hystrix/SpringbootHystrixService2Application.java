c919243cf5098d35fa140ff128c179c9dbbf735f
Vijay Konduru <konduruvijaykumar@gmail.com>
2020-05-10 23:21:50 +0530

    service 2 code

---
+++ springboot-hystrix-service-2/src/main/java/org/pjay/hystrix/SpringbootHystrixService2Application.java
@@ -0,0 +1,13 @@
+package org.pjay.hystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHystrixService2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootHystrixService2Application.class, args);
+	}
+
+}
