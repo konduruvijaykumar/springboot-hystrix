5833168f2f6b6abed7a00b3f9b94cc1b9382750d
Vijay Konduru <konduruvijaykumar@gmail.com>
2020-05-11 13:22:20 +0530

    constants and properties changes

---
+++ springboot-hystrix-service-1/src/main/java/org/pjay/hystrix/ApplicationConstants.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package org.pjay.hystrix;
+
+/**
+ * @author vijayk
+ *
+ */
+public class ApplicationConstants {
+	
+	public static final String API_HOST_URL = "http://localhost:8282/";
+	public static final String URL_SERVICE2_TIMEOUT = API_HOST_URL + "s2-hystrix-timeout-param/{timeOutMilliSec}";
+	public static final String URL_SERVICE2_SUCCESS = API_HOST_URL + "s2-hystrix-always-success";
+	public static final String URL_SERVICE2_FAILURE = API_HOST_URL + "s2-hystrix-always-failure";
+	public static final String URL_SERVICE2_RANDOM_SUCCESS_FAILURE = API_HOST_URL + "s2-hystrix-random-success-failure";
+	public static final String URL_SERVICE2_EXCEPTION = API_HOST_URL + "s2-hystrix-always-exception";
+	public static final String URL_SERVICE2_ERRORTHRESHOLDPERCENTAGE = API_HOST_URL + "s2-hystrix-errorthresholdpercentage/{timeOutMilliSec}";
+	public static final String URL_SERVICE2_REQUESTVOLUMETHRESHOLD = API_HOST_URL + "s2-hystrix-requestvolumethreshold/{timeOutMilliSec}";
+	// Below two constants are not used
+	public static final String HYSTRIX_EXECUTION_TIMEOUT_IN_MILLISECONDS = "execution.isolation.thread.timeoutInMilliseconds";
+	public static final String HYSTRIX_TIMEOUT_VALUE = "6000";
+	
+}
