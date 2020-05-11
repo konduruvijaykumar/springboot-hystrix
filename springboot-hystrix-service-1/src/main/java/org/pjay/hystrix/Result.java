0058e4706a6660db1d83070d9f668576801ec028
Vijay Konduru <konduruvijaykumar@gmail.com>
2020-05-11 10:46:19 +0530

    controller code changes

---
+++ springboot-hystrix-service-1/src/main/java/org/pjay/hystrix/Result.java
@@ -0,0 +1,70 @@
+/**
+ * 
+ */
+package org.pjay.hystrix;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author vijayk
+ *
+ */
+@Component
+public class Result {
+
+	private String message;
+	private String status;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((message == null) ? 0 : message.hashCode());
+		result = prime * result + ((status == null) ? 0 : status.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Result other = (Result) obj;
+		if (message == null) {
+			if (other.message != null)
+				return false;
+		} else if (!message.equals(other.message))
+			return false;
+		if (status == null) {
+			if (other.status != null)
+				return false;
+		} else if (!status.equals(other.status))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Result [message=" + message + ", status=" + status + "]";
+	}
+
+}
--- springboot-hystrix-service-1/src/main/java/org/pjay/hystrix/SpringbootHystrixService1Controller.java
+++ springboot-hystrix-service-1/src/main/java/org/pjay/hystrix/SpringbootHystrixService1Controller.java
@@ -3,6 +3,9 @@
  */
 package org.pjay.hystrix;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -11,5 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class SpringbootHystrixService1Controller {
+	
+	@GetMapping(value = { "/", "/home", "/hello" })
+	public ResponseEntity<Result> hello() {
+		Result result = new Result();
+		result.setMessage("Hi there from Springboot Hystrix Service1");
+		return new ResponseEntity<>(result, HttpStatus.OK);
+	}
 
 }
