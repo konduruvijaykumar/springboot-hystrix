1c5a15d22da5f8db2e9b0ab2f02d1dc28928d24a
Vijay Konduru <konduruvijaykumar@gmail.com>
2020-05-11 23:51:01 +0530

    rest template bean changes

--- springboot-hystrix-service-1/src/main/java/org/pjay/hystrix/SpringbootHystrixService1Application.java
+++ springboot-hystrix-service-1/src/main/java/org/pjay/hystrix/SpringbootHystrixService1Application.java
@@ -2,6 +2,8 @@ package org.pjay.hystrix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @author vijayk
@@ -12,6 +14,11 @@ public class SpringbootHystrixService1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootHystrixService1Application.class, args);
+	}
+
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
 	}
 
 }
