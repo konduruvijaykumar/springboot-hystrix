0bdf101a532aee51b82e1002d1c8190d3d0a277d
Vijay Konduru <konduruvijaykumar@gmail.com>
2020-05-12 01:16:19 +0530

    s2 controller methods - mappings

--- springboot-hystrix-service-2/src/main/java/org/pjay/hystrix/SpringbootHystrixService2Controller.java
+++ springboot-hystrix-service-2/src/main/java/org/pjay/hystrix/SpringbootHystrixService2Controller.java
@@ -3,9 +3,12 @@
  */
 package org.pjay.hystrix;
 
+import java.util.Random;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -15,10 +18,79 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SpringbootHystrixService2Controller {
 	
+	Random random = new Random();
+	
 	@GetMapping(value = { "/", "/home", "/hello" })
 	public ResponseEntity<Result> hello() {
 		Result result = new Result();
 		result.setMessage("Hi there from Springboot Hystrix Service2");
+		return new ResponseEntity<>(result, HttpStatus.OK);
+	}
+	
+	@GetMapping("s2-hystrix-timeout-param/{timeOutMilliSec}")
+	public ResponseEntity<Result> s2HystrixTimeoutParam(@PathVariable long timeOutMilliSec) throws InterruptedException {
+		Result result = new Result();
+		Thread.sleep(timeOutMilliSec);
+		result.setMessage("Response from s2HystrixTimeoutParam("+timeOutMilliSec+") method");
+		return new ResponseEntity<>(result, HttpStatus.OK);
+	}
+	
+	@GetMapping("s2-hystrix-always-success")
+	public ResponseEntity<Result> s2HystrixAlwaysSuccess() {
+		Result result = new Result();
+		result.setMessage("Response from s2HystrixAlwaysSuccess() method");
+		return new ResponseEntity<>(result, HttpStatus.OK);
+	}
+	
+	@GetMapping("s2-hystrix-always-failure")
+	public ResponseEntity<Result> s2HystrixAlwaysFailure() throws InterruptedException {
+		Result result = new Result();
+		Thread.sleep(18000); // more value than defined in service1 for timeout
+		result.setMessage("Response from s2HystrixAlwaysSuccess() method");
+		return new ResponseEntity<>(result, HttpStatus.OK);
+	}
+	
+	@GetMapping("s2-hystrix-random-success-failure")
+	public ResponseEntity<Result> s2HystrixRandomSuccessFailure() throws InterruptedException {
+		Result result = new Result();
+		if(random.nextBoolean()) {
+			Thread.sleep(18000); // more value than defined in service1 for timeout
+			result.setMessage("Response from s2HystrixRandomSuccessFailure() method failed");
+		}else {
+			result.setMessage("Response from s2HystrixRandomSuccessFailure() method successful");
+		}
+		
+		return new ResponseEntity<>(result, HttpStatus.OK);
+	}
+	
+	@GetMapping("s2-hystrix-always-exception")
+	public ResponseEntity<Result> s2HystrixAlwaysException() {
+		Result result = new Result();
+		result.setMessage("s2HystrixAlwaysException() method failed with exception");
+		throw new RuntimeException("s2HystrixAlwaysException() method failed with exception");
+	}
+	
+	@GetMapping("s2-hystrix-errorthresholdpercentage/{timeOutMilliSec}")
+	public ResponseEntity<Result> s2HystrixErrorThresholdPercentage(@PathVariable long timeOutMilliSec) throws InterruptedException {
+		Result result = new Result();
+		Thread.sleep(timeOutMilliSec);
+		result.setMessage("Response from s2HystrixErrorThresholdPercentage("+timeOutMilliSec+") method");
+		return new ResponseEntity<>(result, HttpStatus.OK);
+	}
+	
+	@GetMapping("s2-hystrix-requestvolumethreshold/{timeOutMilliSec}")
+	public ResponseEntity<Result> s2HystrixRequestVolumeThreshold(@PathVariable long timeOutMilliSec) throws InterruptedException {
+		Result result = new Result();
+		Thread.sleep(timeOutMilliSec);
+		result.setMessage("Response from s2HystrixRequestVolumeThreshold("+timeOutMilliSec+") method");
+		return new ResponseEntity<>(result, HttpStatus.OK);
+	}
+	
+	@GetMapping("s2-hystrix-command-prop/{timeOutMilliSec}")
+	public ResponseEntity<Result> s2HystrixCommandProp(@PathVariable long timeOutMilliSec) throws InterruptedException {
+		Result result = new Result();
+		Thread.sleep(timeOutMilliSec);
+		result.setMessage("Response from s2HystrixCommandProp("+timeOutMilliSec+") method");
 		return new ResponseEntity<>(result, HttpStatus.OK);
 	}
 
