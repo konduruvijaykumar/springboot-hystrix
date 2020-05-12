79018b36a38659dd7050f0003bc61b1436c917c9
Vijay Konduru <konduruvijaykumar@gmail.com>
2020-05-12 21:18:13 +0530

    hystrix dashboard changes

---
+++ springboot-hystrix-dashboard/src/test/java/org/pjay/hystrix/SpringbootHystrixDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pjay.hystrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootHystrixDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
