430728ec69e9f8271de9763acf9fd736839726c7
Vijay Konduru <konduruvijaykumar@gmail.com>
2020-05-13 10:42:56 +0530

    Hystrix dashboard and properties change

--- springboot-hystrix-dashboard/src/main/java/org/pjay/hystrix/SpringbootHystrixDashboardApplication.java
+++ springboot-hystrix-dashboard/src/main/java/org/pjay/hystrix/SpringbootHystrixDashboardApplication.java
@@ -2,8 +2,12 @@ package org.pjay.hystrix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
+@EnableHystrixDashboard
+//https://spring.io/blog/2016/10/27/spring-tips-circuit-breakers
+//URL for dashboard is http://localhost:8383/hystrix
 public class SpringbootHystrixDashboardApplication {
 
 	public static void main(String[] args) {
